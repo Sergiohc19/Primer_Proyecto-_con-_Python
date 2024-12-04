850f59a8b7569d2487e0e1610adfd170b9241afb
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2024-12-04 18:18:53 +0100

    Generador de nombres completado

---
+++ Primer_Proyecto.py
@@ -0,0 +1,1 @@
+print("El nombre de tu ecommerce \nes '" + input("¿En qué nicho está tu empresa?: ") + "  " + input("¿Cuál es tu animal favorito?: ") + "'\n¡¡¡Felicitaciones ya tienes tu nombre de marca!!!")
